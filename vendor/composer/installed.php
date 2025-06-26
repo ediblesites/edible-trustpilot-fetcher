8e919e52d8ae04f28af26e7e0c76f2f7d6f5e4bb
Adam Marash <adam@ediblesites.com>
2025-06-26 10:45:26 +0100

    Include vendor files for distribution

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ediblesites/edible-trustpilot-fetcher',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e8faf0c3dad07aabf7f514b5ebc4d8bd8a6d7381',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ediblesites/edible-trustpilot-fetcher' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e8faf0c3dad07aabf7f514b5ebc4d8bd8a6d7381',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => '3.9.2',
+            'version' => '3.9.2.0',
+            'reference' => 'efbb7953f72a433086335b249292f280dd43ddfe',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
